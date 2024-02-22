dcd1511521fd9ba56af33f634625b19cc58d147f
Harrie_Mburu <40524846+Harrieson@users.noreply.github.com>
2024-02-22 13:58:20 +0300

    Add all apps

---
+++ lead/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Lead(models.Model):
+    LOW = 'low'
+    MEDIUM = 'medium'
+    HIGH = 'high'
+
+    CHOICES_PRIORITY = (
+        (LOW, 'Low'),
+        (MEDIUM, 'Medium'),
+        HIGH, 'High'
+    )
+    priority = models.CharField(max_length=10, choices=CHOICES_PRIORITY, default=MEDIUM)
+    created_by = models.ForeignKey(User, related_name='leads', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
