af30d8273e6039e77d8f2e2de744687f85308cb8
Harrie_Mburu <40524846+Harrieson@users.noreply.github.com>
2024-02-24 23:52:39 +0300

    Leads management

--- lead/views.py
+++ lead/views.py
@@ -1,7 +1,31 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from .forms import AddLeadForm
+from .models import Lead
 
 
 @login_required
+def leads_list(request):
+    leads = Lead.objects.filter(created_by=request.user)
+
+    return render(request, 'lead/leads_list.html', {
+        'leads': leads
+    })
+
+@login_required
 def add_lead(request):
-    return render(request, 'lead/add_lead.html')
+    if request.method == 'POST':
+        form = AddLeadForm(request.POST)
+        if form.is_valid():
+            lead = form.save(commit=False)
+            lead.created_by = request.user
+            lead.save()
+
+            return redirect('dashboard')
+    else:
+
+        form = AddLeadForm()
+    return render(request, 'lead/add_lead.html', {
+        'form': form
+    })
